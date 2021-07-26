d48c1b93c440f8cc2790117866b0badece35ed5a
Hannah Amundson <hannah.amundson@data.world>
2021-07-25 19:13:33 -0500

    Adding in a comment

--- src/main/java/patterns/factory/FactoryMain.java
+++ src/main/java/patterns/factory/FactoryMain.java
@@ -3,6 +3,12 @@ package patterns.factory;
 import patterns.factory.connection.Connection;
 import patterns.factory.connection.FirstFactory;
 
+/**
+ * The factory method lets you specify which type of class you will be selection. You know to use this when you are
+ * using multiple new operators to create different types so you put it into a method and were then having to change the
+ * method. The factory pattern allows you to separate changeable code from the core code so you aren't having to
+ * constantly modify the changing code.
+ */
 public class FactoryMain {
     public static void run() {
         FirstFactory factory = new FirstFactory("Oracle");
